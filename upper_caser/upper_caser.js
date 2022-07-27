5d4f6f8a215f1acd87bb855f51ce50d45eea6c10
Jack Knowles <jackmhknowles@gmail.com>
2022-07-27 20:44:58 +0100

    can't get more concise for upper_caser

--- upper_caser/upper_caser.js
+++ upper_caser/upper_caser.js
@@ -5,8 +5,10 @@ const UpperCaser = function (words) {
 // ATTEMPT 1 _________ ❌ kept returning undefined
 
 UpperCaser.prototype.toUpperCase = function () {
-   const upper = this.words.map(word => word.toUpperCase())
-}
+   const upper = this.words.map(word => {
+    return word.toUpperCase()
+    });
+};
 
 
  
@@ -14,21 +16,11 @@ UpperCaser.prototype.toUpperCase = function () {
 // // ATTEMPT 2 BASIC _________ ✅
 
 // UpperCaser.prototype.toUpperCase = function () {
-//     capitals = []
-//     const upper = this.words.forEach(word => {
-//          capitals.push(word.toUpperCase());
-//       });
-//    return capitals; 
-//  };
-
-// OR 
-
-// UpperCaser.prototype.toUpperCase = function () {
-//     capitals = []
+//     capitalised = []
 //     const upper = this.words.map(word => {
-//          capitals.push(word.toUpperCase());
+//          capitalised.push(word.toUpperCase());
 //       });
-//    return capitals; 
+//    return capitalised; 
 //  };
  
 
